84aadb0c815afd074a7aad826a79976e9715ffad
MANIKANDAN S <smani2887@gmail.com>
2024-05-29 15:26:51 +0530

    add two string-split and join method

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
-let arr = [1,2,3,4,5,6];
-let even = [];
-let odd = [];
-
-for( let i=0; i<arr.length; i++){
-    if(i % 2 == 0){
-        even.push(arr[i])
-        
-
-    } else {
-        odd.push(arr[i])
-    }
-    
-}
-
-    console.log(even)
-    console.log(odd)
+var names = 'MANI KANDAN'.split(" ").join("")
+console.log(names);
