07780ebe0b39f11983995946a85e4b066304282f
MANIKANDAN S <smani2887@gmail.com>
2024-05-29 15:32:22 +0530

    string is using split method

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
+let frist=[];
+let last=[];
 var names = 'MANI KANDAN'.split(" ").join("")
-console.log(names);
+for(let i=0; i<names.length; i++){
+    if(i % 2 == 0){
+        frist.push(names[i]);
+    } else {
+        last.push(names[i])
+    }
+}
+console.log(frist);
+console.log(last)
